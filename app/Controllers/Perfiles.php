a60590a861b7c1e72985d8c68d64fab2aa4f3707
Klly97 <kjjimenez97@gmail.com>
2024-11-03 19:00:41 -0500

    Initial commit

---
+++ app/Controllers/Perfiles.php
@@ -0,0 +1,28 @@
+<?php 
+
+namespace App\Controllers;
+
+use App\Models\PersonaModel;
+use CodeIgniter\Session\Session; 
+
+class Perfiles extends BaseController
+{
+
+    public function RegistroMaquinas()
+    {
+        return view('agregarMaquinas');
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+?>
